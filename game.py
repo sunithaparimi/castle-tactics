77e2a25fe3b0d5b523f6fbd1a6301bd48f4e1da1
AnnamareddiSuhitha <142532715+AnnamareddiSuhitha@users.noreply.github.com>
2025-06-27 11:36:34 +0530

    Update game.py

--- game.py
+++ game.py
@@ -918,5 +918,8 @@ def purchase_hints():
 
 
 
+
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
+
